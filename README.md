49de362a7cf7bbaff92a6155bf71c4c5ebb2ff58
Bach Ngoc Son <sonbn1.viettelrd.vn>
2018-02-13 22:08:42 +0700

    Add tls support

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 gen key:
 openssl req -x509 -newkey rsa:2048 -keyout pkey.pem -out cert.pem -days 3650
+
+client setup:
+var exampleWebSocket = new WebSocket("wss://localhost.localdomain:3000/");
+exampleWebSocket.onmessage = function (event) {
+    console.log(event);
+};
+exampleWebSocket.onclose = function (event) {
+    console.log(event);
+};
+
+client usage:
+exampleWebSocket.send("[Protobuf serialized service message]")
--- main.cpp
+++ main.cpp
@@ -7,20 +7,30 @@
 
 int main(int argc, char* argv[]) {
   (void) argc; (void) argv;
-  uS::TLS::Context tls_context = uS::TLS::createContext("./crt.pem", "./pkey.pem", "quenmatroi");
+  uS::TLS::Context tls_context = uS::TLS::createContext("./cert.pem", "./pkey.pem");
+  if (!tls_context) {
+    std::cerr << "Cannot create tls context" << std::endl;
+    return EXIT_FAILURE;
+  }
+
   std::vector<std::thread *> threads(std::thread::hardware_concurrency());
   std::transform(threads.begin(), threads.end(), threads.begin(), [&](std::thread *t) {
     (void) t;
     return new std::thread([&]() {
       uWS::Hub h;
-      h.onMessage([](uWS::WebSocket<uWS::SERVER> *ws, char *message, size_t length, uWS::OpCode opCode) {
+      uWS::Group<uWS::SERVER> *ssl_group = h.createGroup<uWS::SERVER>(uWS::PERMESSAGE_DEFLATE);
+      ssl_group->onMessage([](uWS::WebSocket<uWS::SERVER> *ws, char *message, size_t length, uWS::OpCode opCode) {
         std::cout << "Thread ID: " << std::this_thread::get_id() << ", Addr: " << ws->getAddress().address << ", Port: " << ws->getAddress().port << ": ";
         std::cout.write(message, length);
         std::cout.flush();
         ws->send(message, length, opCode);
       });
-      if (!h.listen(LISTEN_PORT, tls_context, uS::ListenOptions::REUSE_PORT)) {
-        std::cerr << "Failed to listen on port " <<  LISTEN_PORT << std::endl;
+      ssl_group->onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t length, size_t remainingBytes) {
+        res->end("Hello, World!", 13);
+      });
+      if (!h.listen(LISTEN_PORT, tls_context, uS::ListenOptions::REUSE_PORT, ssl_group)) {
+        std::cerr << "Failed to listen to Websocket Secure on port " <<  LISTEN_PORT << std::endl;
+        return EXIT_FAILURE;
       }
       h.run();
     });
