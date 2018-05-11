f17d2b9344e6057f1fa0240d5bc211ff468d3000
Bach Ngoc Son <sonbn1@viettel.com.vn>
2018-05-12 05:13:57 +0700

    remove http handling

--- main.cpp
+++ main.cpp
@@ -16,23 +16,21 @@ int main(int argc, char* argv[]) {
   std::vector<std::thread *> threads(std::thread::hardware_concurrency());
   std::transform(threads.begin(), threads.end(), threads.begin(), [&](std::thread *t) {
     (void) t;
-    return new std::thread([&]() {
+    return new std::thread([&]() -> int {
       uWS::Hub h;
       uWS::Group<uWS::SERVER> *ssl_group = h.createGroup<uWS::SERVER>(uWS::PERMESSAGE_DEFLATE);
       ssl_group->onMessage([](uWS::WebSocket<uWS::SERVER> *ws, char *message, size_t length, uWS::OpCode opCode) {
         std::cout << "Thread ID: " << std::this_thread::get_id() << ", Addr: " << ws->getAddress().address << ", Port: " << ws->getAddress().port << ": ";
         std::cout.write(message, length);
-        std::cout.flush();
+        std::cout << std::endl;
         ws->send(message, length, opCode);
       });
-//      ssl_group->onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data, size_t length, size_t remainingBytes) {
-//        res->end("Hello, World!", 13);
-//      });
       if (!h.listen(LISTEN_PORT, tls_context, uS::ListenOptions::REUSE_PORT, ssl_group)) {
         std::cerr << "Failed to listen to Websocket Secure on port " <<  LISTEN_PORT << std::endl;
         return EXIT_FAILURE;
       }
       h.run();
+      return EXIT_SUCCESS;
     });
   });
 
